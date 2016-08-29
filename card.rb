1130fc50a551d690720ab03c13ad2a060827b354
Christophe Rezk <christophe.rezk@gmail.com>
2016-08-29 14:36:30 +0000

    Finished building the deck

--- card.rb
+++ card.rb
@@ -1,4 +1,5 @@
 class Card
+
   attr_accessor :rank, :suit
 
   def initialize(rank, suit)
@@ -7,17 +8,45 @@ class Card
   end
 
   def output_card
-    self.output_card
-    #puts "#{self.rank} of #{self.suit}"
+    puts "#{self.rank} of #{self.suit}"
   end
 
   def self.random_card
     Card.new(rand(10), :spades)
   end
+end
+
+class Deck
+
+  def initialize
+    @cards = []
+    @rank = ['Ace',2,3,4,5,6,7,8,9,10,'Jack','Queen','King']
+    @suit = [:spades, :diamonds, :clubs, :hearts]
+    @rank.each do |r|
+      @suit.each do |s|
+        @cards << Card.new(r,s)
+      end
+    end
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
+
+  def output
+    @cards.each do |card|
+      card.output_card
+    end
+  end
+
+  def deal
+    @cards.shift
+  end
 
 end
 
-card = Card.random_card
-card.output_card
-
+deck = Deck.new
+deck.shuffle
+deck.deal 
+deck.output
 
